d97fe82c066802bd6503352bfc83aac6c0e6c6ab
Jos <b1t@mailbox.org>
2026-04-10 13:28:48 +0800

    Initial Commit

---
+++ pinch_control/pinch_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinch_controlApp.swift
+//  pinch_control
+//
+//  Created by Jos on 10/4/26.
+//
+
+import SwiftUI
+
+@main
+struct pinch_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pinch_controlTests/pinch_controlTests.swift
@@ -0,0 +1,19 @@
+//
+//  pinch_controlTests.swift
+//  pinch_controlTests
+//
+//  Created by Jos on 10/4/26.
+//
+
+import Testing
+@testable import pinch_control
+
+struct pinch_controlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
